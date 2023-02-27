6d1e108bc136d14dac7cae85bca959ebac53d447
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-02-27 14:53:32 -0600

    Finished the bom exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,13 @@
 "use strict";
 
-// let skip = Number(prompt('Pick a odd number between 1 and 50.'))
-//
-// for(let i = 1;i < 50; i+=2){
-//     if (i === skip){
-//         console.log(`Yikes skipping ${skip}.`)
-//     }
-//     console.log(`Here is a odd number ${i}`)
-// }
+let skip = ''
+if (skip === ''|| skip < 1 || skip > 50 || skip %2 !== 0){
+    Number(prompt('Pick a odd number between 1 and 50.'))
+}
+
+for(let i = 1;i < 50; i+=2){
+    if (i === skip){
+        console.log(`Yikes skipping ${skip}.`)
+    }
+    console.log(`Here is a odd number ${i}`)
+}
