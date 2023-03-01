cefa8d1270ff1a38f030b6c87a78db26253636fb
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-03-01 15:41:41 -0600

    loops rework

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,15 @@
 "use strict";
 
-let skip = ''
-if (skip === ''|| skip < 1 || skip > 50 || skip %2 !== 0){
-    Number(prompt('Pick a odd number between 1 and 50.'))
+let skip = Number(prompt('Pick a odd number between 1 and 50.'))
+while(skip === ''|| skip < 1 || skip > 50 || skip %2 === 0 || isNaN(skip)){
+    skip = Number(prompt('Pick a odd number between 1 and 50.'))
+    console.log(skip)
 }
 
 for(let i = 1;i < 50; i+=2){
     if (i === skip){
         console.log(`Yikes skipping ${skip}.`)
+        continue
     }
     console.log(`Here is a odd number ${i}`)
 }
